7d907170c18e1a0cb96aaab369947a058e0520e6
javijupra <jdejprado@gmail.com>
2023-01-09 21:43:32 +0000

    initial scraping

---
+++ search_urls.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+for (let year = 1946; year <= 2022; year++)
+{
+    for (let index = 1; index <= 201; index+=100) {
+
+        url_string = `https://digitallibrary.un.org/search?ln=en&cc=Voting+Data&p=&f=&jrec=${index}&rm=&ln=en&sf=&so=d&rg=100&c=Voting+Data&c=&of=btex&fti=0&fct__9=Vote&fct__3=${year}&fti=0`
+
+        fs.appendFileSync('./search_urls.txt', url_string + '\n')
+        
+    }
+}
---
+++ search_urls.sh
@@ -0,0 +1,9 @@
+# https://unix.stackexchange.com/questions/698848/how-to-speed-up-curl
+# time sh -c 'cat search_urls.txt | xargs -P0 -n1 curl'
+    #  grep -oE 'http[}]}'
+
+cat search_urls.txt | xargs -P0 -n1 curl >> results_raw.txt
+
+cat results_raw.txt | grep -oE 'http[^}]+' >> results_urls.txt
+
+awk '!seen[$0]++' results_urls.txt >> clean_results_urls.txt  
