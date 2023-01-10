27e73f788288436ca88fbcbdeec7f9ed5b1463df
Javier Prado <jdejprado@gmail.com>
2023-01-09 18:51:28 -0700

    All results

--- search_urls.sh
+++ search_urls.sh
@@ -3,8 +3,9 @@
     #  grep -oE 'http[}]}'
 
 cat search_urls.txt | xargs -P0 -n1 curl > results_raw.txt
+# cat ../search_urls.txt -n | xargs -P0 -n2 curl -o
 
 cat results_raw.txt | grep -oE 'http[^}]+' >> results_urls.txt
 
-awk '!seen[$0]++' results_urls.txt > results_urls.txt  
+awk '!seen[$0]++' results_urls.txt > clean_results_urls_2.txt  
 # awk '!seen[$0]++' results_urls.txt >> clean_results_urls.txt  
